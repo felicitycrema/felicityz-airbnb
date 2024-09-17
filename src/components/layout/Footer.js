1e45c8f931bdc98dad04515c4a14e37ebbb4ec88
Shikwambana <felicity.shikwambana2@gauteng.gov.za>
2024-09-17 02:46:16 +0200

    updated footer, and searchPage

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const footer = () => {
   return (
     <div className="footer">
-        <p>&copy: 2022 AirBnB Clone! No rights reserved - this is s demo</p>
+        <p>&copy: 2022 AirBnB Clone! No rights reserved - this is a demo</p>
         <p> Privacy . Terms . Sitemap . Company Details</p>
     </div>
   )
